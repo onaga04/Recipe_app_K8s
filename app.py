979afcf4cb436cdbfb55969a3b880a12596da733
Ebube Onaga <31213368+onaga04@users.noreply.github.com>
2023-06-30 20:26:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
@@ -18,17 +19,40 @@ recipes = [
 
 @app.route('/')
 def home():
+    start_time = time.time()
+    # Your application logic here
+    # ...
     return render_template('index.html', recipes=recipes)
-
+    response = render_template('index.html', recipes=recipes)
+    end_time = time.time()
+    response_time = end_time - start_time
+    # Log the response time
+    app.logger.info(f"Response time for home route: {response_time} seconds")
+    return response
+    
 @app.route('/recipe/<title>')
 def recipe(title):
+    # Find the recipe with the given title
+    start_time = time.time()
+
+    # Your application logic here
+    # ...
+
     # Find the recipe with the given title
     recipe = next((r for r in recipes if r['title'].lower() == title.lower()), None)
 
     if recipe:
-        return render_template('recipe.html', recipe=recipe)
+        response = render_template('recipe.html', recipe=recipe)
     else:
-        return 'Recipe not found'
+        response = 'Recipe not found'
+
+    end_time = time.time()
+    response_time = end_time - start_time
+
+    # Log the response time
+    app.logger.info(f"Response time for recipe route: {response_time} seconds")
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
